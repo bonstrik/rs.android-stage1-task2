e713e1c19f8664f52c7ebb9f72416d6fcf72e9f4
bonstrik <AK.reply2016@yandex.ru>
2020-04-14 13:06:35 +0300

    5 m

--- src/main/kotlin/subtask3/Abbreviation.kt
+++ src/main/kotlin/subtask3/Abbreviation.kt
@@ -1,9 +1,62 @@
 package subtask3
 
 class Abbreviation {
+}
 
-    // TODO: Complete the following function
-    fun abbreviationFromA(a: String, b: String): String {
-        throw NotImplementedError("Not implemented")
+
+fun Abbreviation.abbreviationFromA(a: String, b: String): String {
+     val a = a.toUpperCase()
+     var i = 0; var j = 0
+     while ((i < b.length) and (j < a.length)) {
+         if (b[i] == a[j]) {
+             i++; j++
+         } else {
+             j++
+         }
+     }
+     if (i < b.length) {
+         return "NO"
+     } else {
+         return "YES"
+     } 
+}
+
+fun main() {
+    val converter = Abbreviation()
+    
+    fun testAbbreviation1() {
+    println("Run1")
+        if ("YES" != converter.abbreviationFromA("daBcd", "ABC"))
+            println("Bad")
     }
+
+    fun testAbbreviation2() {
+    println("Run2")
+        if ("YES" != converter.abbreviationFromA("mnbTy", "BT"))
+            println("Bad")
+    }
+
+    fun testAbbreviation3() {
+    println("Run3")
+        if ("YES" != converter.abbreviationFromA("qWerty", "WY"))
+            println("Bad")
+    }
+
+    fun testAbbreviation4() {
+    println("Run4")
+        if ("NO" != converter.abbreviationFromA("ytrewq", "RY"))
+            println("Bad")
+    }
+
+    fun testAbbreviation5() {
+    println("Run5")
+        if ("NO" != converter.abbreviationFromA("abhfs", "ASCI"))
+            println("Bad")
+    }
+    testAbbreviation1()
+    testAbbreviation2()
+    testAbbreviation3()
+    testAbbreviation4()
+    testAbbreviation5()
+
 }
